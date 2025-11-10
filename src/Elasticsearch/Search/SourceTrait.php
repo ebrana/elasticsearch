ddcbee045574582b8d108b915fb939ab31da9fc5
Martin Chudoba <martin.chudoba@seznam.cz>
2025-11-10 18:07:09 +0100

    [D019897][FIX] phpDoc

--- src/Elasticsearch/Search/SourceTrait.php
+++ src/Elasticsearch/Search/SourceTrait.php
@@ -7,12 +7,12 @@ namespace Elasticsearch\Search;
 trait SourceTrait
 {
     /**
-     * @var string[]|string|bool|null
+     * @var string[]|string|bool|null|array<string, array<int, string>>
      */
     private array|string|bool|null $source = null;
 
     /**
-     * @return bool|string[]|string|null
+     * @return bool|string[]|string|null|array<string, array<int, string>>
      */
     public function getSource(): bool|array|string|null
     {
